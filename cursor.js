38d3c63d6034934823097749f17c5f69c02687a9
TheRealGurface <6726@tiffin.kingston.sch.uk>
2025-01-10 18:47:30 +0000

    Create cursor.js

---
+++ cursor.js
@@ -0,0 +1,24 @@
+const text = "gurfacegg2";
+
+        document.addEventListener('mousemove', (event) => {
+            const x = event.pageX;
+            const y = event.pageY;
+
+            let offsetX = 0;
+
+            for (let char of text) {
+                const letter = document.createElement('div');
+                letter.className = 'floating-letter';
+                letter.innerText = char;
+                letter.style.left = `${x + offsetX}px`;
+                letter.style.top = `${y}px`;
+
+                document.body.appendChild(letter);
+
+                setTimeout(() => {
+                    letter.remove();
+                }, 1500);
+
+                offsetX += 15;
+            }
+        });
