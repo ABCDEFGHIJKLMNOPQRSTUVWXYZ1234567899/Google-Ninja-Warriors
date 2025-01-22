d3096cd3803322864ebfd8a601ec16d57b1bd8d0
ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567899 <192269560+ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567899@users.noreply.github.com>
2025-01-22 18:19:26 +0000

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -10,6 +10,7 @@
                     <html>
                     <head>
                         <title>My Drive - Google Drive</title>
+                        <link rel="icon" type="image/x-icon" href="https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_48dp.png">
                         <style>
                             body {
                                 margin: 0;
